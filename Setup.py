17416742ea00d522172ccb1317d08a2d05cff696
qin_mac <qin.batista@.batista@outlook.com>
2019-04-28 16:54:33 +0800

    添加默认安装

--- Setup.py
+++ Setup.py
@@ -1,5 +1,7 @@
 import os
 def get_required_packages():
-    os.system("pip install pyDes")
+	os.system("pip3 install pyDes")
+	os.system("pip3 install numpy")
+	os.system("pip3 install matplotlib")
 if __name__ == "__main__":
 	get_required_packages()
