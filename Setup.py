a5a3afe65d038eab3ede7509e784e906e90fb577
qin_mac <qin.batista@.batista@outlook.com>
2019-05-06 14:31:30 +0800

    merge matthew pip requirement.txt, users can install all 3rd part by setup.py

--- Setup.py
+++ Setup.py
@@ -1,7 +1,7 @@
 import os
 def get_required_packages():
-	os.system("pip3 install pyDes")
-	os.system("pip3 install numpy")
-	os.system("pip3 install matplotlib")
+	os.system("pip3 install -r requirements.txt")
+	os.system("pip install -r requirements.txt")
+	os.system("pip.exe install -r requirements.txt")
 if __name__ == "__main__":
 	get_required_packages()
