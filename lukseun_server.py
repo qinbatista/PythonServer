215332bc9b9f3d4bb932be78f59598e24f6a8556
Matthew Fernandez <mattdfernandez@gmail.com>
2019-05-16 14:28:19 -0700

    Increased the number of workers given to the lukseun_server ProcessPool. In general I believe 2x the number of available cores on the machine should be a good number of workers. If no argument is given to the LukseunServer object, it will default to the number of cores on the machine.

--- lukseun_server.py
+++ lukseun_server.py
@@ -26,12 +26,12 @@ COLORS = {'pass' : '\033[92m', 'fail' : '\033[91m', 'end' : '\033[0m', \
 		'ylw' : '\033[1;33;40m', 'grn' : '\033[1;32;40m'}
 
 class LukseunServer:
-	def __init__(self, host: str = '', port: int = 8888):
+	def __init__(self, host: str = '', port: int = 8888, max_workers: int = None):
 		self._host = host
 		self._port = port
 		self._header_tool = AnalysisHeader.Header()
 		self._wtr = WorkingTimeRecoder.WorkingTimeRecoderClass()
-		self._pool = concurrent.futures.ProcessPoolExecutor()
+		self._pool = concurrent.futures.ProcessPoolExecutor(max_workers = max_workers)
 
 
 	async def run(self) -> None:
@@ -88,7 +88,7 @@ class LukseunServer:
 
 
 async def main() -> None:
-	server = LukseunServer()
+	server = LukseunServer(max_workers = 16)
 	await server.run()
 
 if __name__ == '__main__':
