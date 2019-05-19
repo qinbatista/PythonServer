b5b5735473c70234ef03515b7e2e4b051d126a93
Matthew Fernandez <mattdfernandez@gmail.com>
2019-05-18 23:59:18 -0700

    Increased concurrency with testing platform. Server performs well.

--- testing.py
+++ testing.py
@@ -26,9 +26,34 @@ def send_single_message(message_id: int):
 	asyncio.run(client.send_message(str(d)))
 	print(f"Message #{message_id} took {COLORS['pass']} {time.time() - start} {COLORS['end']} seconds to complete.")
 
+##########################################################################################3
+def new_test_multiple_message(n: int):
+	start = time.time()
+	with multiprocessing.Pool() as pool:
+		for _ in pool.imap_unordered(async_multi_message, range(n)):
+			pass
+	print(f'It took {time.time() - start} seconds to complete {n * 10} messages.')
+
+async def async_send_single_message(message_id: int):
+	client = LukseunClient()
+	d = {'session': 'ACDE48001122', 'function': 'CheckTime', 'random': '744', 'data': {'user_name': 'yupeng', 'gender': 'male', 'email': 'qin@lukseun.com', 'phone_number': '15310568888'}}
+	start = time.time()
+	await client.send_message(str(d))
+	print(f"Message #{message_id} took {COLORS['pass']} {time.time() - start} {COLORS['end']} seconds to complete.")
+
+def async_multi_message(message_id: int):
+	client = LukseunClient()
+	d = {'session': 'ACDE48001122', 'function': 'CheckTime', 'random': '744', 'data': {'user_name': 'yupeng', 'gender': 'male', 'email': 'qin@lukseun.com', 'phone_number': '15310568888'}}
+	#start = time.time()
+	tasks = [asyncio.ensure_future(async_send_single_message(message_id * i)) for i in range(10)]
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(asyncio.gather(*tasks))
+	#print(f"Message #{message_id} took {COLORS['pass']} {time.time() - start} {COLORS['end']} seconds to complete.")
+
 
 def main() -> None:
-	test_multiple_message(int(input('How many messages to send: ')))
+	#test_multiple_message(int(input('How many messages to send: ')))
+	new_test_multiple_message(int(input('How many messages to send (it will be n * 10 so be careful): ')))
 
 
 if __name__ == '__main__':
