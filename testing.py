2f90cfa5d92ccf8040e0531f300a54152445381d
qin_mac <qin.batista@.batista@outlook.com>
2019-06-03 12:02:32 +0800

    fix “” to '

--- testing.py
+++ testing.py
@@ -23,14 +23,15 @@ def test_multiple_message(n: int):
 
 def send_single_message(message_id: int):
 	client = LukseunClient(client_type="workingcat",host="192.168.1.183")
-	d = {'session': '', 'function': 'login', 'random': '744', 'data': {'unique_id': 'ACDE480011228888', 'account': '', 'password': ''}}
+	d = {'session':'', 'function':'login', 'random':'-906', 'data':{'unique_id':'4E71A852-60CA-51EF-B8CC-C80CD627A180', 'account':'', 'password':''}}
 	#d = {'session': 'ACDE480011228888_session', 'function': 'check_time', 'random': '744', 'data': ""}
 	#d = {'session': 'ACDE480011228888_session', 'function': 'get_staff_current_status', 'random': '744', 'data': ""}
 
 	# d = {'session': '', 'function': 'login', 'random': '744', 'data': {'unique_id': 'ACDE480011228888', 'account': 'a', 'password': 'a'}}#aliya login
 	# d = {'session': '', 'function': 'create_account', 'random': '744', 'data': {'unique_id': 'ACDE480011228888', 'account': 'a', 'password': 'a',"ip":"","user_name":"","gender":"","birth_day":"","last_time_login":"","registration_time":""}}#aliya create account
 	start = time.time()
-	asyncio.run(client.send_message(str(d)))
+	newstring  =  str(d).replace("'","\"")
+	asyncio.run(client.send_message(newstring))
 	print(f"Message #{message_id} took {COLORS['pass']} {time.time() - start} {COLORS['end']} seconds to complete.")
 
 ##########################################################################################3
@@ -51,7 +52,7 @@ async def async_send_single_message(message_id: int) -> float:
 	client = LukseunClient()
 	d = {'session': 'ACDE48001122', 'function': 'login', 'random': '744', 'data': {'user_name': 'yupeng', 'gender': 'male', 'email': 'qin@lukseun.com', 'phone_number': '15310568888'}}
 	start = time.time()
-	await client.send_message(str(d))
+	await client.send_message(str(d).replace("'","\""))
 	end = time.time()
 	print(f"Message #{message_id} took {COLORS['pass']} {end - start} {COLORS['end']} seconds to complete.")
 	return end - start
