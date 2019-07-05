2ec1b562de0c27c9cf4afbf3a5a95d8d7c89fcc3
houyao <2428437133@qq.com>
2019-07-05 14:33:20 +0800

    修改session关键字为token关键字，整理了该文件的代码

--- Application/GameAliya/account_module.py
+++ Application/GameAliya/account_module.py
@@ -7,117 +7,138 @@ import threading
 import pymysql
 import datetime;
 import random
+
+
 def PythonLocation():
 	return os.path.dirname(os.path.realpath(__file__))
-from Utility import LogRecorder,EncryptionAlgorithm
+
+
+from Utility import LogRecorder, EncryptionAlgorithm
 from Utility.LogRecorder import LogUtility as Log
 from Utility.sql_manager import game_aliya as gasql
 from Utility.AnalysisHeader import message_constructor as mc
+
 DESKey = "67891234"
 DESVector = "6789123467891234"
 
-MessageList=[
+MessageList = [
 	"{\"status\":\"0\",\"message\":\"success\"}",
 	"{\"status\":\"1\",\"message\":\"failed\",\"time\":\"%s\"}"
 ]
+
+
 class LoginSystemClass():
+
 	def __init__(self, *args, **kwargs):
 		pass
-	def _login(self,message_info):
+
+	def _login(self, message_info):
 		"""
-		user send login command, we return message with session to user
+		user send login command, we return message with token to user
 		"""
-		message_dic  = eval(message_info)
+		message_dic = eval(message_info)
 		if "data" in message_dic.keys():
 			unique_id = message_dic["data"]["unique_id"]
 			account = message_dic["data"]["account"]
 			password = message_dic["data"]["password"]
-			if account=="":
-				#if users don't have account, auto visitor login
+			if account == "":
+				# if users don't have account, auto visitor login
 				Log("[login_system.py][_login] use unique_id for seesion")
 				return self.__visitor_login(unique_id)
 			else:
-				#if users have account and password, unique id is not usable but keep the value
+				# if users have account and password, unique id is not usable but keep the value
 				Log("[login_system.py][_login] use account for seesion")
-				return self.__account_login(account,password)
+				return self.__account_login(account, password)
 		else:
-			return mc("1","data is null")
+			return mc("1", "data is null")
+
 	def _bind_email(self):
 		pass
+
 	def _bind_phone(self):
 		pass
-	def _bind_account(self,message_info):
+
+	def _bind_account(self, message_info):
 		"""
 		create a new account
 		"""
-		message_dic  = eval(message_info)
+		message_dic = eval(message_info)
 		if "data" in message_dic.keys():
 			unique_id = message_dic["data"]["unique_id"]
-			account   = message_dic["data"]["account"]
-			password  = message_dic["data"]["password"]
-			ip        = message_dic["data"]["ip"]
+			account = message_dic["data"]["account"]
+			password = message_dic["data"]["password"]
+			ip = message_dic["data"]["ip"]
 			user_name = message_dic["data"]["user_name"]
-			gender    = message_dic["data"]["gender"]
+			gender = message_dic["data"]["gender"]
 			birth_day = message_dic["data"]["birth_day"]
 			time = datetime.datetime.now().strftime("%H:%M:%S %Y-%m-%d")
-			last_time_login   = time
+			last_time_login = time
 			registration_time = time
-			sql_result = gasql("select count from userinfo where account='"+account+"'")
-			if len(sql_result)<=0:
-				gasql("INSERT INTO userinfo(unique_id,account,password,ip,user_name,gender,birth_day,last_time_login,registration_time) VALUES ('"+unique_id+"','"+account+"','"+password+"','"+ip+"','"+user_name+"','"+gender+"','"+birth_day+"','"+last_time_login+"','"+registration_time+"')")
+			sql_result = gasql("select count from userinfo where account='" + account + "'")
+			if len(sql_result) == 0:
+				gasql("INSERT INTO userinfo(unique_id, account, password, ip, user_name, gender, birth_day, last_time_login, registration_time) "
+				"VALUES ('" + unique_id + "','" + account + "','" + password + "','" + ip + "','" + user_name + "','" + gender + "','" + birth_day + "','" + last_time_login + "','" + registration_time + "')")
 			else:
-				return mc("1","user name already exists")
+				return mc("1", "user name already exists")
 		else:
-			return mc("1","data is null")
-	def __visitor_login(self,unique_id):
+			return mc("1", "data is null")
+
+	def __visitor_login(self, unique_id):
 		"""
 		user login only with unique_id
 		"""
-		sql_result=gasql("select count(account) from userinfo where unique_id='"+unique_id+"'")
-		if sql_result[0][0]<=1:
-			#if account is not exist try to find session
-			sql_result=gasql("select session from userinfo where unique_id='"+unique_id+"'")
-			if len(sql_result)<=0:
-				#if session is not exist, it is a new user, createa a account for them
-				session = self.__create_session_by_unique_id(unique_id)
-				gasql("INSERT INTO userinfo(unique_id,account,password,session) VALUES ('"+unique_id+"','"+""+"','"+""+"','"+session+"')")
+		sql_result = gasql("select count(account) from userinfo where unique_id='" + unique_id + "'")
+		if sql_result[0][0] <= 1:
+			# if account is not exist try to find token
+			sql_result = gasql("select token from userinfo where unique_id='" + unique_id + "'")
+			if len(sql_result) <= 0:
+				# if token is not exist, it is a new user, createa a account for them
+				token = self.__create_token_by_unique_id(unique_id)
+				gasql("INSERT INTO userinfo(unique_id,account,password,token) VALUES ('" + unique_id + "','" + "" + "','" + "" + "','" + token + "')")
 			else:
-				#if session is exist, just give them session
-				session = str(sql_result[0][0])
+				# if token is exist, just give them token
+				token = str(sql_result[0][0])
 			data = {
-				"session":session,
-				"random":str(random.randint(-1000, 1000))
+				"token": token,
+				"random": str(random.randint(-1000, 1000))
 			}
-			return mc("0","login as visitor",data)
+			return mc("0", "login as visitor", data)
 		else:
-			#if account is exist but use visitor login, ask user use account to login
-			return mc("1","this phone is already binded a account,please login as account")
+			# if account is exist but use visitor login, ask user use account to login
+			return mc("1", "this phone is already binded a account,please login as account")
+
 	def __account_login(self, account, password):
 		"""
 		user login with account and password
 		"""
-		sql_result=gasql("select * from userinfo where account='"+account +"' and password='"+password+"'")
-		if len(sql_result)<=0:
-			return mc("1","account is not exist")
+		sql_result = gasql("select * from userinfo where account='" + account + "' and password='" + password + "'")
+		if len(sql_result) <= 0:
+			return mc("1", "account is not exist")
 		else:
-			data={"session":self.__create_session_by_account(account,password),"random":str(random.randint(-1000, 1000))}
-			return mc("0","login as account",data)
-	def __create_session_by_unique_id(self,unique_id):
+			data = {"token": self.__create_token_by_account(account, password),
+			        "random": str(random.randint(-1000, 1000))}
+			return mc("0", "login as account", data)
+
+	def __create_token_by_unique_id(self, unique_id):
 		"""
-		return a session to user, right now just add _session with unique id, matthew will make really session
+		return a token to user, right now just add _token with unique id, matthew will make really token
 		"""
-		return unique_id+"_session"
-	def __create_session_by_account(self,account,password):
+		return unique_id + "_token"
+
+	def __create_token_by_account(self, account, password):
 		"""
-		return a session to user, right now just add _session with account id, matthew will make really session
+		return a token to user, right now just add _token with account id, matthew will make really token
 		"""
-		sql_result=gasql("select session from userinfo where  account='"+account +"' and password='"+password+"'")
-		if sql_result[0][0]=="":
-			session= account+"_session"
-			gasql("UPDATE userinfo SET session='"+session+"' WHERE account='"+account +"' and password='"+password+"'")
-			return session
+		sql_result = gasql(
+			"select token from userinfo where  account='" + account + "' and password='" + password + "'")
+		if sql_result[0][0] == "":
+			token = account + "_token"
+			gasql(
+				"UPDATE userinfo SET token='" + token + "' WHERE account='" + account + "' and password='" + password + "'")
+			return token
 		else:
 			return sql_result[0][0]
+
+
 if __name__ == "__main__":
 	pass
-
