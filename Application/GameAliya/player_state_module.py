0705b2e839a887781ee7ca7cc7be20a93b224d1c
qin yu peng <qin.batista@outlook.com>
2019-07-09 20:32:59 +0800

    增加体力模版

--- Application/GameAliya/player_state_module.py
+++ Application/GameAliya/player_state_module.py
@@ -1,12 +1,12 @@
 import sys
 import json
-import time
 import os
 import codecs
 import threading
 import pymysql
 import random
-
+import time
+from datetime import datetime
 
 def PythonLocation():
 	return os.path.dirname(os.path.realpath(__file__))
@@ -24,9 +24,45 @@ class PlayerStateSystemClass():
 		self.item_list_count = 0
 		self.recover_time = 30
 		self.full_energy = 10
-	def _decrease_energy(self,msg):
-		print("_decrease_energy")
-		return ""
+	def _decrease_energy(self,message_info):
+		message_dic = eval(message_info)
+		if "data" in message_dic.keys():
+			cost_energy = message_dic["data"]["energy"]
+			sql_result = gasql("select energy, energy_recover_time from player_status where unique_id='" + self.unique_id + "'")
+			remain_energy = int(sql_result[0][0])
+			energy_time = sql_result[0][1]
+			cost_energy_int = int(cost_energy)
+			if sql_result[0][1]=="":
+				if remain_energy >=0:
+					if remain_energy>=self.full_energy:
+						# if energy is more than cast, reduce energy,if energy over 10, record time
+						Log("[PlayerStateSystemClass][_decrease_energy] remain_energy>=self.full_energy")
+						gasql("UPDATE player_status SET " + "energy" + "=" + str(remain_energy) + "-" + str(cost_energy) + " WHERE unique_id='" + self.unique_id + "'")
+						return mc("0", "decrease energy success, reamian energy is over full energy")
+					else:
+						Log("[PlayerStateSystemClass][_decrease_energy] remain_energy<=self.full_energy")
+						gasql("UPDATE player_status SET " + "energy" + "=" + str(remain_energy) + "-" + str(cost_energy) + ", energy_recover_time = '"+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+"' WHERE unique_id='" + self.unique_id + "'")
+						return mc("0", "decrease energy success, remain energy is lessing than full energy, start recovering energy")
+				else:
+					return mc("1", "energy is not enough")
+			else:
+				if remain_energy-cost_energy_int >=0:
+					y = datetime.strptime(energy_time, '%Y-%m-%d %H:%M:%S')
+					z = datetime.strptime(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), '%Y-%m-%d %H:%M:%S')
+					diff = z-y
+					recover_energy = int(diff.seconds/60/20)
+					total_energy = recover_energy+remain_energy
+					if total_energy>=10:
+						gasql("UPDATE player_status SET " + "energy" + "=" + str(10) + "-" + str(cost_energy) + ", energy_recover_time = '"+""+"' WHERE unique_id='" + self.unique_id + "'")
+						return mc("0", "energy is over full energy, remove time recoerd")
+					else:
+						gasql("UPDATE player_status SET " + "energy" + "=" + str(total_energy) + "-" + str(cost_energy) + " WHERE unique_id='" + self.unique_id + "'")
+						return mc("0", "energy is recovering")
+				else:
+					return mc("1", "energy is not enough")
+		else:
+			return mc("1", "data is null")
+
 	def _increase_energy(self,msg):
 		print("_increase_energy")
 		return ""
@@ -40,7 +76,7 @@ class PlayerStateSystemClass():
 
 	def __check_table(self, unique_id):
 		sql_result = gasql("select count(unique_id) from player_status where  unique_id='" + unique_id + "'")
-		if len(sql_result) == 0:
+		if sql_result[0][0] <= 0:
 			gasql("INSERT INTO player_status(unique_id) VALUES ('" + unique_id + "')")
 
 
--- testing.py
+++ testing.py
@@ -22,18 +22,16 @@ MESSAGE_LIST = [ {'token':'', 'function':'login', 'random':'-906', 'data':{'uniq
 				 {'token':token, 'function':'skill_level_up', 'random':'-906', 'data':{'skill_id':'m1_level', 'scroll_id':'scroll_skill_30'}},
 				 {'token':token, 'function':'get_skill', 'random':'-906', 'data':{'skill_id':'m1_level'}},
 				 {'token':token, 'function':'increase_supplies', 'random':'-906', 'data':{'scroll_skill_10':'10','scroll_skill_30':'1'}},
-				 {'token':token, 'function':'get_all_skill_level', 'random':'-906', 'data':""},
 				 {'token':token, 'function':'get_all_supplies', 'random':'-906', 'data':""},
-				 # {"token":"4E71A852-60CA-51EF-B8CC-C80CD627A180_token", "function":"increase_supplies", "random":"603", "data":{"scroll_skill_10":"5", "scroll_skill_30":"5", "scroll_skill_100":"5", "iron":"5", "diamonds":"5", "coin":"5", "weapon1_segment":"5", "weapon2_segment":"5", "weapon3_segment":"5", "weapon4_segment":"5", "weapon5_segment":"5", "weapon6_segment":"5"}},
 				 {"token":token, "function":"increase_supplies", "random":"603", "data":{"scroll_skill_10":"5", "scroll_skill_30":"5", "scroll_skill_100":"5", "iron":"5", "diamonds":"5", "coin":"5", "weapon1_segment":"5", "weapon2_segment":"5", "weapon3_segment":"5", "weapon4_segment":"5", "weapon5_segment":"5", "weapon6_segment":"5"}},
 				 {'token':token, 'function':'random_gift_skill', 'random':'-906', 'data':""},
-				 # {"token":"4E71A852-60CA-51EF-B8CC-C80CD627A180_token", "function":"get_skill", "random":"973", "data":{"skill_id":"m11"}},
+				 {"token":"4E71A852-60CA-51EF-B8CC-C80CD627A180_token", "function":"get_skill", "random":"973", "data":{"skill_id":"m11"}},
 				 {"token":token, "function":"get_skill", "random":"973", "data":{"skill_id":"m11"}},
 				 {'token':token, 'function':'level_up_scroll', 'random':'-906', 'data':{"scroll_skill_30":"3"}},
-				 {'token':token, 'function':'level_up_weapon_level','random':'-906', 'data':{"weapon1":"1020"}},
-				 {'token':token, 'function':'level_up_weapon_passive_skill','random':'-906', 'data':{"weapon1":"passive_skill_2_level"}},
-				 {'token':token, 'function':'reset_weapon_skill_point','random':'-906', 'data':{"weapon1":"100"}},
-				 {'token':token, 'function':'level_up_weapon_star','random':'-906', 'data':{"weapon1":"30"}},
+				 {'token':token, 'function':'level_up_weapon_level','random':'-906', 'data':{"weapon1":"1020"}},#data: [weapon1] means which weapon you want to level up, [1020] means how much iron you want to cast, we set 20 iron to level up in server, if you send 1020 iron, server will level up weapon to 51.(max weapon level is 100)
+				 {'token':token, 'function':'level_up_weapon_passive_skill','random':'-906', 'data':{"weapon1":"passive_skill_2_level"}},#data: [weapon1] means which weapons' passive skill  you want to level up, [passive_skill_2_level] means skill id, right now passive skill only can be level up one by one.
+				 {'token':token, 'function':'reset_weapon_skill_point','random':'-906', 'data':{"weapon1":"100"}},#data: [weapon1] means which weapon you want to reset skill, [100] means cost resources, right now it is coin in our code,it will be change in furture, make sure it is easy change.
+				 {'token':token, 'function':'level_up_weapon_star','random':'-906', 'data':{"weapon1":"30"}},# data: [weapon1] means which weapon you want to increase star, [30] means how much weapon segment weapon need to level up, it's just reference, server will handle really message, righ now level up weapon start cost 30 segment, every level increase 30 segment(1:30,2:60,3:90), make sure the cost segment value can be change easily(star also means number of this weapon).
 				 {'token':token, 'function':'get_all_weapon','random':'-906', 'data':"null"},
 				 {'token':token, 'function':'random_gift_segment','random':'-906', 'data':{"weapon_kind": "100"}},
 				 {'token':token, 'function':'pass_level','random':'-906', 'data':{"customs_clearance_time": "1"}},
@@ -49,7 +47,7 @@ GET_ALL_SKILL_LEVEL=5
 GET_ALL_SUPPLIES=6
 ALL_SUPPLIES_ADD5= 7
 RANDOM_GIFT_SKILL=8
-# GET_SKILL = 9
+GET_SKILL = 9
 SCROLL_LEVEL_UP = 10
 LEVEL_UP_WEAPON =11
 SKILL_UP_WEAPON =12
