42f3be6306f332c3c121e5f4f3c234c6977d49b4
Matthew Fernandez <mattdfernandez@gmail.com>
2019-07-10 12:12:02 +0800

    Levelup weapon and levelup passive functions working. Changed response format

--- Application/GameAliya/WeaponManager.py
+++ Application/GameAliya/WeaponManager.py
@@ -11,47 +11,89 @@ from aiohttp import ClientSession
 
 BAG_MANAGER_BASE_URL = 'http://localhost:8082'
 
+
+
 class WeaponUpgradeError(Exception):
-	pass
+	def __init__(self, code: int, message: str = ''):
+		self.code = code
+		self.message = message
 
 
 class WeaponManager:
 	def __init__(self, standard_iron_count = 20, standard_segment_count = 30):
 		self._standard_iron_count = standard_iron_count
 		self._standard_segment_count = standard_segment_count
+
+		self._valid_passive_skills = ['passive_skill_1_level', 'passive_skill_2_level', 'passive_skill_3_level', 'passive_skill_4_level']
+
 		# This is the connection pool to the SQL server. These connections stay open
 		# for as long as this class is alive. 
 		self._pool = tormysql.ConnectionPool(max_connections = 10, host = '127.0.0.1', user = 'root', passwd = 'lukseun', db = 'aliya', charset = 'utf8')
 
 
 	# levels up a particular weapon. costs iron.
-	async def level_up_weapon_level(self, unique_id: str, weapon: str, iron: int):
-
-		star = await self._get_weapon_star(unique_id, weapon)
-		if star == 0:
-			raise WeaponUpgradeError('User does not have that weapon')
-
-		weapon_level = await self._get_weapon_level(unique_id, weapon)
-		if weapon_level == 100:
-			raise WeaponUpgradeError('Weapon has reached max level!')
-
-		skill_upgrade_number = int(iron) // self._standard_iron_count
+	# returns the data payload
+	async def level_up_weapon_level(self, unique_id: str, weapon: str, iron: int) -> dict:
 		async with ClientSession() as session:
+			star = await self._get_weapon_star(unique_id, weapon)
+			if star == 0:
+				raise WeaponUpgradeError(1, 'User does not have that weapon')
+
+			row = await self._get_row_by_id(weapon, unique_id)
+
+			if row[1] == 100:
+				raise WeaponUpgradeError(9, 'Weapon has reached max level!')
+
 			async with session.post(BAG_MANAGER_BASE_URL + '/get_iron', data = {'unique_id' : unique_id}) as resp:
 				resp = await resp.json(content_type='text/json')
 				current_iron = resp['iron']
-		if skill_upgrade_number > 0 and (current_iron // self._standard_iron_count) >= skill_upgrade_number:
-			if (weapon_level + skill_upgrade_number) > 100:
-				skill_upgrade_number = 100 - weapon_level
-			weapon_level += skill_upgrade_number
+			skill_upgrade_number = int(iron) // self._standard_iron_count
+			if skill_upgrade_number == 0 or (current_iron // self._standard_iron_count) < skill_upgrade_number:
+				raise WeaponUpgradeError(2, 'Insufficient materials, upgrade failed!')
+			
+
+			# calculate resulting levels and used iron
+			if (row[1] + skill_upgrade_number) > 100:
+				skill_upgrade_number = 100 - row[1]
+			row[1] += skill_upgrade_number
+			row[6] += skill_upgrade_number
+
+			# remove the iron from the account
+			async with session.post(BAG_MANAGER_BASE_URL + '/remove_iron', data = {'unique_id' : unique_id, 'amount' : self._standard_iron_count * skill_upgrade_number}) as resp:
+				resp = await resp.json(content_type='text/json')
+				remaining_iron = resp['remaining']
+
+			# update the weapon level on the account
+			await self._set_weapon_level_up_data(unique_id, weapon, row[1], row[6])
+			return {'weapon_bag1' : row, 'item1' : ['iron', remaining_iron]}
+
 
 
 
 
 
 	# levels up a particular passive skill. costs skill points.
-	async def level_up_weapon_passive_skill(self, unique_id: str, weapon: str, passive_skill: str):
-		pass
+	async def level_up_weapon_passive_skill(self, unique_id: str, weapon: str, passive_skill: str) -> dict:
+		weapon_star = await self._get_weapon_star(unique_id, weapon)
+		if weapon_star == 0:
+			raise WeaponUpgradeError(1, 'User does not have that weapon')
+
+		if passive_skill not in self._valid_passive_skills:
+			raise WeaponUpgradeError(9, 'Passive skill does not exist')
+
+		row = await self._get_row_by_id(weapon, unique_id)
+
+		if row[6] <= 0:
+			raise WeaponUpgradeError(2, 'Insufficient skill points, upgrade failed')
+		row[6] -= 1
+		row[2 + self._valid_passive_skills.index(passive_skill)] += 1
+
+		await self._set_passive_skill_level_up_data(unique_id, weapon, passive_skill, row[2 + self._valid_passive_skills.index(passive_skill)], row[6])
+		return {'weapon_bag1' : row}
+
+
+
+
 
 	# resets all weapon passive skill points. refunds all skill points back. costs coins.
 	async def reset_weapon_skill_points(self, unique_id: str, weapon: str):
@@ -62,21 +104,29 @@ class WeaponManager:
 		pass
 
 
+
+
+
+
+
 	async def _get_weapon_star(self, unique_id: str, weapon: str) -> int:
 		data = await self._execute_statement("SELECT `" + str(weapon) + "` FROM weapon_bag WHERE unique_id='" + str(unique_id) + "';")
 		if () in data or data == None:
 			return 0
 		return data[0][0]
 
-	async def _get_weapon_level(self, unique_id: str, weapon: str) -> int:
-		data = await self._execute_statement("SELECT weapon_level FROM `" + str(weapon) + "` WHERE unique_id='" + str(unique_id) + "';")
-		if () in data or data == None:
-			return 0
-		return data[0][0]
+	async def _set_weapon_level_up_data(self, unique_id: str, weapon: str, weapon_level: int, skill_point: int):
+		await self._execute_statement("UPDATE `" + str(weapon) + "` SET weapon_level='" + str(weapon_level) + "', skill_point='" + str(skill_point) + "' WHERE unique_id='" + str(unique_id) + "';")
 
 
+	async def _set_passive_skill_level_up_data(self, unique_id: str, weapon: str, passive_skill: str, skill_level: int, skill_points: int):
+		await self._execute_statement("UPDATE `" + str(weapon) + "` SET " + passive_skill + "='" + str(skill_level) + "', skill_point='" + str(skill_points) + "' WHERE unique_id='" + str(unique_id) + "';")
 
-
+	async def _get_row_by_id(self, table_name: str, unique_id: str) -> list:
+		data = await self._execute_statement("SELECT * FROM `" + str(table_name) + "` WHERE unique_id='" + str(unique_id) + "';")
+		if () in data: # this should never happen
+			return None
+		return list(data[0])
 
 
 
