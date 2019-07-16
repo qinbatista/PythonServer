566b11f23fa1fdef77ce5063efdd0e859cb1164d
qin yu peng <qin.batista@outlook.com>
2019-07-16 16:45:27 +0800

    合并代码到当前名字下的python文件

--- Application/GameAliya/FunctionModule/_01_Merge_Code.py
+++ Application/GameAliya/FunctionModule/_01_Merge_Code.py
@@ -34,7 +34,7 @@ def get_routes(file_path):
 	global routes_code
 	all_the_text = file_object.readlines()
 	is_find_key=False
-	print("finding routes:"+file_path)
+	# print("finding routes:"+file_path)
 	for i in all_the_text:
 		if is_find_key == False and i.find("@ROUTES.post")!=-1:
 			is_find_key = True
@@ -51,9 +51,8 @@ def get_class_method(file_path):
 	all_the_text = file_object.readlines()
 	is_find_key=False
 	start_continue=False
-	print("finding class method:"+file_path)
+	# print("finding class method:"+file_path)
 	for i in all_the_text:
-		print(i.count("\t"))
 		if is_find_key == False and i.find("\tasync def")!=-1:
 			is_find_key = True
 			class_method_code.append(i)
@@ -61,8 +60,6 @@ def get_class_method(file_path):
 			if i.find("_execute_statement")!=-1 or i.find("message_typesetting")!=-1:
 				start_continue = True
 			elif start_continue == True:
-				print("["+str(i.find("\t")))
-				print("["+str(i.count("\t")))
 				if i.find("\t")<=0 and i.count("\t")<=1:
 					start_continue=False
 					class_method_code.append(i)
@@ -73,7 +70,24 @@ def get_class_method(file_path):
 		elif is_find_key==True and i.find("\t")!=0 and i != "\n" and i != "\r":
 			break
 	return class_method_code
-def start_merge(_path):
+def merge_content_to_manager(file_name):
+	file_object = open(PythonLocation()+"/../"+file_name+".py",encoding="utf-8")
+	print(PythonLocation()+"/../"+file_name+".py")
+	new_file_content = []
+	all_the_text = file_object.readlines()
+	is_find_key=False
+	# print("finding routes:"+file_path)
+	for i in all_the_text:
+		if is_find_key == False and i.find("class")==0:
+			new_file_content.append(i)
+			new_file_content = new_file_content+class_method_code
+		if is_find_key == False and i.find("@ROUTES.get")==0:
+			new_file_content = new_file_content+routes_code
+			new_file_content.append(i)
+	with open(PythonLocation()+"/../"+file_name+".py", 'w',encoding="utf-8") as json_file:
+		for i in new_file_content:
+			json_file.writelines(i)
+def search_merge_content(_path):
 	file_list = FindAll(_path)
 	for file_name in file_list:
 		route_list = get_routes(file_name)
@@ -89,7 +103,8 @@ def merge_code():
 	list_folder = FindAllInFolder(PythonLocation())
 	for i in list_folder:
 		if os.path.isdir(i):
-			start_merge(i)
+			search_merge_content(i)
+			merge_content_to_manager(i[i.rfind("/")+1:])
 
 if __name__ == '__main__':
 	merge_code()
