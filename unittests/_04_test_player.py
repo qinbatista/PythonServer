b2b10b6bdbcd41951159fe3d6116b186242f8445
houyao <2428437133@qq.com>
2019-07-17 19:22:30 +0800

    测试了技能模块，level_up_skill方法有问题

--- unittests/_04_test_player.py
+++ unittests/_04_test_player.py
@@ -8,39 +8,61 @@ MANAGER_BAG_BASE_URL = 'http://localhost:' + CONFIG['bag_manager']['port']
 MANAGER_WEAPON_BASE_URL = 'http://localhost:' + CONFIG['_01_Manager_Weapon']['port']
 MANAGER_PLAYER_BASE_URL = 'http://localhost:' + CONFIG['_04_Manager_Player']['port']
 MANAGER_LEVEL_BASE_URL = 'http://localhost:' + CONFIG['stage_manager']['port']
+MANAGER_SKILL_BASE_URL = 'http://localhost:' + CONFIG['skill_manager']['port']
 
 
-def test_try_remove_coin():
-	result = requests.post(MANAGER_BAG_BASE_URL + '/try_remove_coin', data={'unique_id': "4", "value": 255})
-	print(str(result.text))
-	
-	
-def test_try_remove_iron():
-	result = requests.post(MANAGER_BAG_BASE_URL + '/try_remove_iron', data={'unique_id': "4", "value": -50})
-	print(str(result.text))
-	
-	
-def test_try_remove_diamond():
-	# result = requests.post(MANAGER_BAG_BASE_URL + '/try_remove_diamond', data={'unique_id': "4", "value": -1000})
-	result = requests.post(MANAGER_PLAYER_BASE_URL + '/try_remove_diamond', data={'unique_id': "4", "value": -1000})
+def try_coin():
+	result = requests.post(MANAGER_BAG_BASE_URL + '/try_coin', data={'unique_id': "4", "value": 255})
 	print(str(result.text))
 
 
-def test_level_up_weapon():
+def try_iron():
+	result = requests.post(MANAGER_BAG_BASE_URL + '/try_iron', data={'unique_id': "4", "value": -50})
+	print(str(result.text))
+
+
+def try_diamond():
+	# result = requests.post(MANAGER_BAG_BASE_URL + '/try_diamond', data={'unique_id': "4", "value": -1000})
+	result = requests.post(MANAGER_PLAYER_BASE_URL + '/try_diamond', data={'unique_id': "4", "value": -1000})
+	print(str(result.text))
+
+
+def level_up_weapon():
 	# result = requests.post(MANAGER_WEAPON_BASE_URL + '/level_up_weapon', data={'unique_id': "4", "weapon": "weapon1", "iron": 1000})
 	result = requests.post(MANAGER_PLAYER_BASE_URL + '/level_up_weapon', data={'unique_id': "4", "weapon": "weapon1", "iron": 1000})
 	print(str(result.text))
 
 
-def test_pass_stagel():
+def pass_stage():
 	# result = requests.post(MANAGER_LEVEL_BASE_URL + '/pass_stage', data={'unique_id': "4", "stage": 1})
 	result = requests.post(MANAGER_PLAYER_BASE_URL + '/pass_stage', data={'unique_id': "4", "stage": 1})
 	print(str(result.text))
 
 
+def get_skill():
+	result = requests.post(MANAGER_SKILL_BASE_URL + '/get_skill', data={'unique_id': "4", "skill_id": "m1_level"})
+	# result = requests.post(MANAGER_PLAYER_BASE_URL + '/get_skill', data={'unique_id': "4", "stage": 1})
+	print(str(result.text))
+
+
+def get_all_skill_level():
+	result = requests.post(MANAGER_SKILL_BASE_URL + '/get_all_skill_level', data={'unique_id': "4"})
+	# result = requests.post(MANAGER_PLAYER_BASE_URL + '/get_all_skill_level', data={'unique_id': "4"})
+	print(str(result.text))
+
+
+def level_up_skill():
+	result = requests.post(MANAGER_SKILL_BASE_URL + '/level_up_skill', data={'unique_id': "4", "skill_id": "m1_level",  "scroll_id": "skill_scroll_10"})
+	# result = requests.post(MANAGER_PLAYER_BASE_URL + '/get_all_skill_level', data={'unique_id': "4"})
+	print(str(result.text))
+
+
 if __name__ == "__main__":
-	# test_try_remove_coin()
-	# test_try_remove_iron()
-	# test_try_remove_diamond()
-	# test_level_up_weapon()
-	test_pass_stagel()
+	# try_remove_coin()
+	# try_remove_iron()
+	# try_remove_diamond()
+	# level_up_weapon()
+	# pass_stage()
+	# get_skill()
+	# get_all_skill_level()
+	level_up_skill()
