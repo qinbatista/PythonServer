14526953ec40a6ff34171945c7d12aa129bb4e43
Matthew Fernandez <mattdfernandez@gmail.com>
2019-07-24 15:06:57 +0800

    Removed playerstatemanager and weaponmanager documents, as they were un-needed. GameManager documentation needs to be updated. Added ConfigurationManager documentation.

--- Application/GameAliya/configuration_manager.py
+++ Application/GameAliya/configuration_manager.py
@@ -9,12 +9,15 @@ import threading
 import configparser
 from aiohttp import web
 
+SERVER_CONFIG = './Configuration/server/1.0/server.conf'
 
 MONSTER = './Configuration/client/1.0/monster_config.json'
 REWARD_LIST = './Configuration/client/1.0/stage_reward_config.json'
 ENEMY_LAYOUT = './Configuration/client/1.0/level_enemy_layouts_config.json'
+HANG_REWARD = './Configuration/client/1.0/hang_reward_config.json'
 
 
+MYSQL_DATA = './Configuration/server/1.0/mysql_data_config.json'
 LOTTERY = './Configuration/server/1.0/lottery.json'
 WEAPON = './Configuration/server/1.0/weapon.json'
 SKILL = './Configuration/server/1.0/skill.json'
@@ -29,6 +32,9 @@ class ConfigurationManager:
 		self._read_game_manager_config()
 		self._read_level_enemy_layouts_config()
 		self._read_monster_config()
+		self._read_stage_reward_config()
+		self._read_hang_reward_config()
+		self._read_mysql_data_config()
 	
 	async def get_game_manager_config(self):
 		return self._game_manager_config
@@ -39,7 +45,14 @@ class ConfigurationManager:
 	async def get_monster_config(self):
 		return self._monster_config
 
+	async def get_stage_reward_config(self):
+		return self._stage_reward_config
 
+	async def get_hang_reward_config(self):
+		return self._hang_reward_config
+
+	async def get_mysql_data_config(self):
+		return self._mysql_data_config
 
 	def _read_game_manager_config(self):
 		reward_list = [v for v in (json.load(open(REWARD_LIST, encoding = 'utf-8'))).values()]
@@ -53,6 +66,15 @@ class ConfigurationManager:
 
 	def _read_monster_config(self):
 		self._monster_config = json.load(open(MONSTER, encoding = 'utf-8'))
+
+	def _read_stage_reward_config(self):
+		self._stage_reward_config = json.load(open(REWARD_LIST, encoding = 'utf-8'))
+
+	def _read_hang_reward_config(self):
+		self._hang_reward_config = json.load(open(HANG_REWARD, encoding = 'utf-8'))
+
+	def _read_mysql_data_config(self):
+		self._mysql_data_config = json.load(open(MYSQL_DATA, encoding = 'utf-8'))
 
 	def _start_timer(self, seconds: int):
 		threading.Timer(seconds, self._refresh_configurations).start()
@@ -79,27 +101,35 @@ def _json_response(body: str = '', **kwargs) -> web.Response:
 
 @ROUTES.get('/get_game_manager_config')
 async def __get_game_manager_config(request: web.Request) -> web.Response:
-	data = await MANAGER.get_game_manager_configuration()
-	return _json_response(data)
+	return _json_response(await MANAGER.get_game_manager_config())
 
 @ROUTES.get('/get_level_enemy_layouts_config')
 async def __get_level_enemy_layouts_config(request: web.Request) -> web.Response:
-	data = await MANAGER.get_level_enemy_layouts_config()
-	return _json_response(data)
+	return _json_response(await MANAGER.get_level_enemy_layouts_config())
 
 @ROUTES.get('/get_monster_config')
 async def __get_monster_config(request: web.Request) -> web.Response:
-	data = await MANAGER.get_monster_config()
-	return _json_response(data)
+	return _json_response(await MANAGER.get_monster_config())
 
+@ROUTES.get('/get_stage_reward_config')
+async def __get_stage_reward_config(request: web.Request) -> web.Response:
+	return _json_response(await MANAGER.get_stage_reward_config())
 
+@ROUTES.get('/get_hang_reward_config')
+async def __get_hang_reward_config(request: web.Request) -> web.Response:
+	return _json_response(await MANAGER.get_hang_reward_config())
 
+@ROUTES.get('/get_mysql_data_config')
+async def __get_mysql_data_config(request: web.Request) -> web.Response:
+	return _json_response(await MANAGER.get_mysql_data_config())
 
 
 def run():
 	app = web.Application()
 	app.add_routes(ROUTES)
-	web.run_app(app, port=8002)
+	server_config = configparser.ConfigParser()
+	server_config.read(SERVER_CONFIG)
+	web.run_app(app, port=server_config.getint('configuration_manager', 'port'))
 
 
 if __name__ == '__main__':
