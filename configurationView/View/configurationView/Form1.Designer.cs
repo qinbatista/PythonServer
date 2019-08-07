1c08d95edcdb6a7acc046aa6c9026aaaba285ffd
houyao <2428437133@qq.com>
2019-08-07 19:28:38 +0800

    删除了pyqt做的框架，修改为C#的方法模板

---
+++ configurationView/View/configurationView/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace configurationView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.version = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.VersionOption = new System.Windows.Forms.ComboBox();
+            this.add = new System.Windows.Forms.Button();
+            this.FunctionOption = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // version
+            // 
+            this.version.AutoSize = true;
+            this.version.Location = new System.Drawing.Point(130, 31);
+            this.version.Name = "version";
+            this.version.Size = new System.Drawing.Size(44, 18);
+            this.version.TabIndex = 0;
+            this.version.Text = "版本";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numericUpDown3);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.comboBox4);
+            this.panel1.Controls.Add(this.comboBox3);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 191);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1178, 505);
+            this.panel1.TabIndex = 1;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(833, 282);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(81, 28);
+            this.numericUpDown3.TabIndex = 24;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(765, 284);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 18);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "数量：";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1075, 387);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(79, 39);
+            this.button5.TabIndex = 22;
+            this.button5.Text = "添加";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(960, 387);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(79, 39);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "删除";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1075, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 39);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "添加";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(960, 274);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 39);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "删除";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(161, 394);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(755, 26);
+            this.comboBox4.TabIndex = 18;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(161, 281);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(571, 26);
+            this.comboBox3.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 397);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 18);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "怪物生成点";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 18);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "怪物生成列表";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(778, 159);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(213, 22);
+            this.radioButton1.TabIndex = 14;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "是否等待上一波怪打完";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(400, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 18);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "每一波结束等待时间";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(576, 159);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(81, 28);
+            this.numericUpDown2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 18);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "每波怪的总生成时间";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(202, 161);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(81, 28);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1075, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 39);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "添加";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(960, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 39);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "删除";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(216, 55);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(700, 26);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(26, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(148, 26);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // VersionOption
+            // 
+            this.VersionOption.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.VersionOption.FormattingEnabled = true;
+            this.VersionOption.Location = new System.Drawing.Point(216, 28);
+            this.VersionOption.Name = "VersionOption";
+            this.VersionOption.Size = new System.Drawing.Size(700, 26);
+            this.VersionOption.TabIndex = 2;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(960, 21);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(79, 39);
+            this.add.TabIndex = 3;
+            this.add.Text = "添加";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // FunctionOption
+            // 
+            this.FunctionOption.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FunctionOption.FormattingEnabled = true;
+            this.FunctionOption.Location = new System.Drawing.Point(216, 81);
+            this.FunctionOption.Name = "FunctionOption";
+            this.FunctionOption.Size = new System.Drawing.Size(700, 26);
+            this.FunctionOption.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "功能";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "生效时间";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(262, 130);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 28);
+            this.dateTimePicker.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 694);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FunctionOption);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.VersionOption);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.version);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "View";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label version;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox VersionOption;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.ComboBox FunctionOption;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ configurationView/View/configurationView/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace configurationView
+{
+    public partial class Form1 : Form
+    {
+        //DirectoryInfo di1 = new DirectoryInfo(Directory.GetCurrentDirectory());
+        string json_version = "..//..//..//..//..//configuration//config_timer_setting.json";
+        StreamReader stream;
+        JsonTextReader reader;
+        JObject json;
+        public Form1()
+        {
+            InitializeComponent();
+            stream = File.OpenText(json_version);
+            reader = new JsonTextReader(stream);
+            json = (JObject)JToken.ReadFrom(reader);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(var date in json)
+            {
+                //this.VersionOption.Items.Add(date.Key + ":" + date.Value);
+                this.VersionOption.Items.Add(date.Value["server"]);
+            }
+        }
+        private void Add_Click(object sender, EventArgs e)
+        {
+            String version_str = Interaction.InputBox("请输入版本号，将会拷贝最新版本作为当前版本", "输入密码", "");
+            try
+            {
+                float version = float.Parse(version_str);
+                DialogResult result = MessageBox.Show(text: "是否确定使用版本号：" + version_str, caption: "确认提示", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (result.ToString().Equals("Yes"))
+                {
+                    this.FunctionOption.Items.Add(version);
+                }
+            }
+            catch
+            {
+                MessageBox.Show(text: "版本信息输入错误！", caption: "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
