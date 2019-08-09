5cfd195aa2b799084638273f71439aa36d3ff466
houyao <2428437133@qq.com>
2019-08-09 16:08:30 +0800

    修复了删除朋友的关键字

--- game_manager.py
+++ game_manager.py
@@ -1548,7 +1548,7 @@ class GameManager:
 		data = await self._execute_statement(world, f'SELECT * FROM friend WHERE unique_id = "{unique_id}" AND friend_id = "{friend_id}";')
 		if len(data) != 0:
 			data = await self._execute_statement(world, f'DELETE FROM friend WHERE unique_id = "{unique_id}" AND friend_id = "{friend_id}";')
-			return self._message_typesetting(0, 'delete friend success')
+			return self._message_typesetting(0, 'delete friend success', data={"remaining": {"friend_name": friend_name}})
 		else:
 			return self._message_typesetting(98, 'you do not have this friend')
 
