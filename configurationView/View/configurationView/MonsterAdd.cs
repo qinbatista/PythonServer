67fa3ba82a198de75d7e3ffaa03fcc65e5514b02
houyao <2428437133@qq.com>
2019-08-09 11:59:02 +0800

    添加了配置文件可视化工具的部分功能

---
+++ configurationView/View/configurationView/MonsterAdd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace configurationView
+{
+    public partial class MonsterAdd : Form
+    {
+        string add_item;
+        JObject client_stage_json;
+        public MonsterAdd(JObject client_stage_json)
+        {
+            InitializeComponent();
+            this.client_stage_json = client_stage_json;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //add_item = comboBox1.SelectedItem.ToString();
+            MessageBox.Show(text: client_stage_json.ToString());
+            //client_stage_json.Add("client_stage_json": client_stage_json["client_stage_json"][0]);
+            textBox1.Text = client_stage_json.ToString();
+        }
+    }
+}
