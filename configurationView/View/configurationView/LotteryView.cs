861aa50ae73b124c6b768ff84ef0e28ca2a27a14
houyao <2428437133@qq.com>
2019-08-16 20:03:49 +0800

    修改了一些主程序代码，完善了之前4个面板的合理运行

---
+++ configurationView/View/configurationView/LotteryView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace configurationView
+{
+    public partial class LotteryView : Form
+    {
+        public LotteryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
