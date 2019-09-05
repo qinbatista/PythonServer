70e84d3e669a61dfd12cd4bfd47bd0c7ade3086d
qin yu peng <qin.batista@outlook.com>
2019-09-05 17:57:38 +0800

    武器逻辑书写完毕，武器升级材料不足时的逻辑还未开始制作

--- unittests/user_behavior_simulation/user_behavior_simulation.py
+++ unittests/user_behavior_simulation/user_behavior_simulation.py
@@ -60,8 +60,8 @@ def run_task(name):
 	get_level_info,get_all_friend_info,get_all_skill_level,get_all_weapon,refresh_all_storage,get_all_roles,get_stage_info,get_monster_info,get_factory_info,get_all_family_info,get_all_mail,get_all_armor_info = call_get_all_info()#加载所有参数信息
 	# call_friend_dialog(get_all_friend_info)#朋友界面
 	# skill_dialog(get_all_skill_level)#技能界面
-	# weapon_dialog(get_all_weapon)#武器界面
-	factory_dialog(refresh_all_storage)#工厂界面
+	weapon_dialog(get_all_weapon)#武器界面
+	# factory_dialog(refresh_all_storage)#工厂界面
 	# get_random_item()#抽奖界面
 	# role_dialog()#角色界面
 	# family_dialog(get_all_family_info)#家族界面*暂时不需要
