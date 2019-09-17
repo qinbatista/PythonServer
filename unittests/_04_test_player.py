6cd4780ce844ac8ef434f7395d69f182563c5e03
houyao <2428437133@qq.com>
2019-09-17 14:47:23 +0800

    进入关卡方法测试，没有出现不扣体力的错误

--- unittests/_04_test_player.py
+++ unittests/_04_test_player.py
@@ -122,7 +122,7 @@ def get_hang_up_reward():
 
 
 def enter_stage(stage: int):
-	result = requests.post('http://localhost:8004/enter_stage', data={"world": 0, 'unique_id': "4", 'stage': stage})
+	result = requests.post('http://localhost:8004/enter_stage', data={"world": 0, 'unique_id': "4", 'stage': 1})
 	print(str(result.text))
 
 
@@ -461,7 +461,7 @@ if __name__ == "__main__":
 	# try_energy()
 	# start_hang_up()
 	# get_hang_up_reward()
-	# enter_stage(stage=1)
+	enter_stage(stage=1)
 	# enter_tower(stage=3)
 	# disintegrate_weapon()
 	# automatically_refresh_store()
@@ -504,7 +504,7 @@ if __name__ == "__main__":
 	# end   ########################################################
 	# enter_stage(stage=1)
 	# enter_tower(stage=1)
-	all_function(unique_id="4")
+	# all_function(unique_id="4")
 
 	# check_boss_status(unique_id="4")
 	# check_boss_status(unique_id="4")
