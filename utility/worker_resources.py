1b53219ecdb0fffc639a69c607cb51ecad52c84e
Matthew Fernandez <mattdfernandez@gmail.com>
2019-09-23 15:10:31 +0800

    added stuff

--- utility/worker_resources.py
+++ utility/worker_resources.py
@@ -11,6 +11,7 @@ The worker provides these resources to the message handler.
 import aiohttp
 import aioredis
 import aiomysql
+import concurrent
 
 class WorkerResources:
 	def __init__(self, cfg):
@@ -18,6 +19,7 @@ class WorkerResources:
 		self.resources = {}
 	
 	async def init(self):
+		self.resources['executor'] = concurrent.futures.ProcessPoolExecutor(max_workers = 1)
 		self.resources['session'] = aiohttp.ClientSession()
 		self.resources['redis'] = await aioredis.create_redis(self.cfg['redis']['addr'])
 		self.resources['db'] = await aiomysql.create_pool(maxsize = 10, host = '192.168.1.102', user = 'root', password = 'lukseun', charset = 'utf8', autocommit = True, db = 'experimental')
@@ -31,6 +33,8 @@ class WorkerResources:
 		await self.resources['accountdb'].wait_closed()
 
 		await self.resources['session'].close()
+
+		self.resources['executor'].shutdown(wait = False)
 
 	def __getitem__(self, key):
 		return self.resources[key]
