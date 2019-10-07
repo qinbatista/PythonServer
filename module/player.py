ee2883ae267b68f58023ef01a7d6192b3d6086fa
Matthew Fernandez <mattdfernandez@gmail.com>
2019-10-07 16:13:15 -0700

    added player.py

---
+++ module/player.py
@@ -0,0 +1,16 @@
+'''
+player.py
+'''
+
+from module import enums
+from module import common
+
+async def accept_gift(uid, key, **kwargs):
+	pass
+
+async def change_name(uid, name, **kwargs):
+	pass
+
+async def get_info(uid, **kwargs):
+	pass
+
