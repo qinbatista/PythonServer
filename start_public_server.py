4018096a277b3e4299e0061053966b1d98773e69
qin yu peng <qin.batista@outlook.com>
2019-10-07 18:49:30 +0800

    添加方便调试到文件

---
+++ start_public_server.py
@@ -0,0 +1,84 @@
+import os
+import time
+import subprocess
+import signal
+import requests
+PythonVersion=""
+def loc():
+	return os.path.dirname(os.path.realpath(__file__))
+
+def GetPythonCommand():
+	global PythonVersion
+	if PythonVersion!="":
+		return PythonVersion
+
+	try:
+		version1 = os.popen("python3.7 --version")
+		if version1.read()!="":
+			PythonVersion="python3.7"
+		print("Your are using python command:"+PythonVersion)
+		return PythonVersion
+	except Exception as e:
+		print(str(e))
+
+	try:
+		version2 = os.popen("python.exe --version")
+		if version2.read()!="":
+			PythonVersion="python.exe"
+		print("Your are using python command:"+PythonVersion)
+		return PythonVersion
+	except Exception as e:
+		print(str(e))
+
+	try:
+		version3 = os.popen("python3 --version")
+		if version3.read()!="":
+			PythonVersion="python3"
+		print("Your are using python command:"+PythonVersion)
+		return PythonVersion
+	except Exception as e:
+		print(str(e))
+
+	try:
+		version4 = os.popen("python --version")
+		if version4.read()!="":
+			PythonVersion="python"
+		print("Your are using python command:"+PythonVersion)
+		return PythonVersion
+	except Exception as e:
+		print(str(e))
+	# print("Version:"+version1.read())
+	# print("show:"+version2.read())
+	# print("show:"+version3.read())
+
+
+def main():
+	processes = []
+	try:
+		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/configuration_manager.py']))
+		time.sleep(1)
+		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/mail_server.py']))
+		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/token_server.py']))
+		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/chat_server.py']))
+		# processes.append(subprocess.Popen(['python', loc() + '/account_manager.py']))
+		# processes.append(subprocess.Popen(['python', loc() + '/game_manager_qin.py']))
+		# processes.append(subprocess.Popen(['python3', loc() + '/game_manager_houyao.py']))
+		# processes.append(subprocess.Popen(['python', loc() + '/worker.py']))
+		# processes.append(subprocess.Popen(['python', loc() + '/gate.py']))
+		time.sleep(0.2)
+		print('Done spawning servers...')
+		while (len(processes) > 0):
+			time.sleep(5)
+	except KeyboardInterrupt:
+		pass
+	finally:
+		for process in processes:
+			process.send_signal(signal.SIGINT)
+			process.wait()
+
+
+
+
+
+if __name__ == '__main__':
+	main()
