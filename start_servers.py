77f55434cd386191a16f7608a59e8246a7a0e07f
houyao <2428437133@qq.com>
2019-10-08 12:57:14 +0800

    完善了start_server适配window的功能

--- start_servers.py
+++ start_servers.py
@@ -4,76 +4,86 @@ import subprocess
 import signal
 import requests
 from socket import *
-udpClient = socket(AF_INET,SOCK_DGRAM) #创建客户端
+
+udpClient = socket(AF_INET, SOCK_DGRAM)  # 创建客户端
+
+
 def loc():
 	return os.path.dirname(os.path.realpath(__file__))
-PythonVersion=""
+
+
+PythonVersion = ""
+
+
 def GetPythonCommand():
 	global PythonVersion
-	if PythonVersion!="":
+	if PythonVersion != "":
 		return PythonVersion
 
 	try:
 		version1 = os.popen("python3.7 --version")
-		if version1.read()!="":
-			PythonVersion="python3.7"
-		print("Your are using python command:"+PythonVersion)
-		return PythonVersion
+		if version1.read() != "":
+			PythonVersion = "python3.7"
+			print("Your are using python command:" + PythonVersion)
+			return PythonVersion
 	except Exception as e:
 		print(str(e))
 
 	try:
 		version2 = os.popen("python.exe --version")
-		if version2.read()!="":
-			PythonVersion="python.exe"
-		print("Your are using python command:"+PythonVersion)
-		return PythonVersion
+		if version2.read() != "":
+			PythonVersion = "python.exe"
+			print("Your are using python command:" + PythonVersion)
+			return PythonVersion
 	except Exception as e:
 		print(str(e))
 
 	try:
 		version3 = os.popen("python3 --version")
-		if version3.read()!="":
-			PythonVersion="python3"
-		print("Your are using python command:"+PythonVersion)
-		return PythonVersion
+		if version3.read() != "":
+			PythonVersion = "python3"
+			print("Your are using python command:" + PythonVersion)
+			return PythonVersion
 	except Exception as e:
 		print(str(e))
 
 	try:
 		version4 = os.popen("python --version")
-		if version4.read()!="":
-			PythonVersion="python"
-		print("Your are using python command:"+PythonVersion)
-		return PythonVersion
+		if version4.read() != "":
+			PythonVersion = "python"
+			print("Your are using python command:" + PythonVersion)
+			return PythonVersion
 	except Exception as e:
 		print(str(e))
-	# print("Version:"+version1.read())
-	# print("show:"+version2.read())
-	# print("show:"+version3.read())
+
+
+# print("Version:"+version1.read())
+# print("show:"+version2.read())
+# print("show:"+version3.read())
 
 
 def get_host_ip():
-    try:
-        s = socket(AF_INET,SOCK_DGRAM)
-        s.connect(('8.8.8.8', 80))
-        ip = s.getsockname()[0]
-    finally:
-        s.close()
-    return ip
+	try:
+		s = socket(AF_INET, SOCK_DGRAM)
+		s.connect(('8.8.8.8', 80))
+		ip = s.getsockname()[0]
+	finally:
+		s.close()
+	return ip
+
 
 def main():
 	processes = []
 	try:
 		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/configuration_manager.py']))
 		time.sleep(1)
-		#processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/mail_server.py']))
-		#processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/token_server.py']))
-		#processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/account_manager.py']))
-		#processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/game_manager_qin.py']))
-		#processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/game_manager_houyao.py']))
-		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/worker.py',get_host_ip()]))
-		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/gate.py',get_host_ip()]))
+		# processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/mail_server.py']))
+		# processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/token_server.py']))
+		# processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/account_manager.py']))
+		# processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/game_manager_qin.py']))
+		# processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/game_manager_houyao.py']))
+		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/worker.py', get_host_ip()]))
+		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/gate.py', get_host_ip()]))
 		time.sleep(0.2)
 		print('Done spawning servers...')
 		while (len(processes) > 0):
@@ -84,9 +94,6 @@ def main():
 		for process in processes:
 			process.send_signal(signal.SIGINT)
 			process.wait()
-
-
-
 
 
 if __name__ == '__main__':
