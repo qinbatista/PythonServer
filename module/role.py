9ac5334ebec60c271f22a23b57b1022409eae184
Matthew Fernandez <mattdfernandez@gmail.com>
2019-10-09 20:19:17 -0700

    simplified get_all_role to remove unsued passives and skillpoint fields

--- module/role.py
+++ module/role.py
@@ -33,15 +33,7 @@ async def level_up_star(uid, rid, **kwargs):
 	return common.mt(0, 'success', {'rid' : rid.value, 'star' : star + 1, 'seg' : segment - cost})
 
 async def get_all(uid, **kwargs):
-	roles = await _get_all_role_info(uid, **kwargs)
-	passives = await _get_all_role_passives(uid, **kwargs)
-	for role in roles:
-		for pid in enums.RolePassive:
-			try:
-				role[f'p{str(pid.value)}'] = passives[role['rid']][pid.value]
-			except KeyError:
-				role[f'p{str(pid.value)}'] = 0 
-	return common.mt(0, 'success', {'roles' : roles})
+	return common.mt(0, 'success', {'roles' : await _get_all_role_info(uid, **kwargs)})
 
 async def get_config(**kwargs):
 	return common.mt(0, 'success', {'seg' : STANDARD_SEGMENT, 'exp_pot' : STANDARD_EXP_POT_COUNT})
@@ -53,9 +45,10 @@ async def _get_role_info(uid, rid, *args, **kwargs):
 	return (True, data[0]) if data != () else (False, ())
 
 async def _get_all_role_info(uid, **kwargs):
-	data = await common.execute(f'SELECT rid, star, level, skillpoint, segment FROM role WHERE uid = "{uid}";', **kwargs)
-	return [{'rid' : r[0], 'star' : r[1], 'level' : r[2], 'sp' : r[3], 'seg' : r[4]} for r in data]
+	data = await common.execute(f'SELECT rid, star, level, segment FROM role WHERE uid = "{uid}";', **kwargs)
+	return [{'rid' : r[0], 'star' : r[1], 'level' : r[2], 'seg' : r[3]} for r in data]
 
+# unused right now
 async def _get_all_role_passives(uid, **kwargs):
 	data = await common.execute(f'SELECT rid, pid, level FROM rolepassive WHERE uid = "{uid}";', **kwargs)
 	passives = defaultdict(dict)
