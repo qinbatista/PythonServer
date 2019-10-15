08be20539b7b70020781159f3023e74a583309fa
houyao <2428437133@qq.com>
2019-10-15 15:01:27 +0800

    完善了获取挂机奖励的方法get_hang_up_reward

--- module/stage.py
+++ module/stage.py
@@ -297,6 +297,8 @@ async def get_hang_up_reward(uid, **kwargs):
 	get_hang_up_rewards = []
 	hang_stage = await get_progress(uid, 'hangstage', **kwargs)
 	probability_reward = kwargs['hang_rewards']['probability_reward']  # self._hang_reward["probability_reward"]
+	stages = [int(s) for s in kwargs['hang_rewards'].keys() if s.isdigit()]
+	if hang_stage not in stages: hang_stage = max(stages)
 	hang_stage_rewards = kwargs['hang_rewards'][str(hang_stage)]  # self._hang_reward[str(hang_stage)]
 	delta_time = datetime.strptime(current_time, '%Y-%m-%d %H:%M:%S') - datetime.strptime(hang_up_time, '%Y-%m-%d %H:%M:%S')
 	minute = int(delta_time.total_seconds()) // 60
