cabf7b1a2228d466a4899d77940395a32458da23
houyao <2428437133@qq.com>
2019-10-27 14:12:03 +0800

    将变参写成位置参数

--- unittests/user_behavior_simulation/module_18_darkmarket.py
+++ unittests/user_behavior_simulation/module_18_darkmarket.py
@@ -16,47 +16,35 @@ def send_tcp_message(msg):
 	return asyncio.get_event_loop().run_until_complete(lukseun.send_message(str(msg).replace("'", "\"")))
 
 def login_decoration(func):
-	def wrapper(*args, **kwargs):
-		func(*args, **kwargs) if kwargs.__contains__("world") else (lambda response=send_tcp_message({'function': 'login_unique', 'data': {'unique_id': '1'}}): func(*args, **{'token': response['data']['token'], 'world': 0}))()
+	def wrapper(world=0, token=None, *args, **kwargs):
+		func(world, token, *args, **kwargs) if token else (lambda response=send_tcp_message({'function': 'login_unique', 'data': {'unique_id': '1'}}): func(world, response['data']['token'], *args, **kwargs))()
 	return wrapper
 
 @login_decoration
-def automatically_refresh(**kwargs):
-	response = send_tcp_message({'world': kwargs['world'], 'function': 'automatically_refresh', 'data': {'token' : kwargs['token']}})
+def get_all_market(world, token, **kwargs):
+	response = send_tcp_message({'world': world, 'function': 'get_all_market', 'data': {'token' : token}})
 	logger.debug(response)
 
 @login_decoration
-def free_refresh(**kwargs):
-	response = send_tcp_message({'world': kwargs['world'], 'function': 'free_refresh', 'data': {'token' : kwargs['token']}})
+def refresh_market(world, token, **kwargs):
+	response = send_tcp_message({'world': world, 'function': 'refresh_market', 'data': {'token' : token}})
 	logger.debug(response)
 
 @login_decoration
-def diamond_refresh(**kwargs):
-	response = send_tcp_message({'world': kwargs['world'], 'function': 'diamond_refresh', 'data': {'token' : kwargs['token']}})
+def darkmarket_transaction(world, token, pid, **kwargs):
+	response = send_tcp_message({'world': world, 'function': 'darkmarket_transaction', 'data': {'token' : token, 'pid': pid}})
 	logger.debug(response)
 
-@login_decoration
-def darkmarket_transaction(pid, **kwargs):
-	response = send_tcp_message({'world': kwargs['world'], 'function': 'darkmarket_transaction', 'data': {'token' : kwargs['token'], 'pid': pid}})
-	logger.debug(response)
-
-def darkmarket_dialog(pid, **kwargs):
-	automatically_refresh(**kwargs)
-	free_refresh(**kwargs)
-	diamond_refresh(**kwargs)
-	darkmarket_transaction(pid, **kwargs)
+def darkmarket_dialog(world, token, pid, **kwargs):
+	get_all_market(world, token, **kwargs)
+	refresh_market(world, token, **kwargs)
+	darkmarket_transaction(world, token, pid, **kwargs)
 
 
 if __name__ == '__main__':
-	# response = send_tcp_message({'function': 'login_unique', 'data': {'unique_id': '1'}})
-	# kwargs = {"world": 0, "token": response['data']['token']}
-	# automatically_refresh(**kwargs)
-	# free_refresh(**kwargs)
-	# automatically_refresh(**{'token': response['data']['token'], 'world': 0})
-	# automatically_refresh()
-	# free_refresh()
-	diamond_refresh()
-	# darkmarket_transaction(1)
+	get_all_market()
+	# refresh_market()
+	# darkmarket_transaction(pid=1)
 
 
 
