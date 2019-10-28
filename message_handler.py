17b1cb80ebe952a42e40b7d6fc703775423a2728
Matthew Fernandez <mattdfernandez@gmail.com>
2019-10-28 17:09:37 +0800

    added set_armor_factory

--- message_handler.py
+++ message_handler.py
@@ -379,6 +379,10 @@ class MessageHandler:
 	async def _activate_wishing_pool_factory(self, data: dict) -> str:
 		return await factory.activate_wishing_pool(data['data']['unique_id'], int(data['data']['wid']), **data)
 
+	async def _set_armor_factory(self, data: dict) -> str:
+		return await factory.set_armor(data['data']['unique_id'], int(data['data']['aid']), **data)
+
+
 	async def _get_config_factory(self, data: dict) -> str:
 		return common.mt(0, 'success', data['config']['factory'])
 
@@ -736,6 +740,7 @@ FUNCTION_LIST = {
 	'increase_worker_factory' : MessageHandler._increase_worker_factory,
 	'decrease_worker_factory' : MessageHandler._decrease_worker_factory,
 	'activate_wishing_pool_factory' : MessageHandler._activate_wishing_pool_factory,
+	'set_armor_factory' : MessageHandler._set_armor_factory,
 	'get_config_factory' : MessageHandler._get_config_factory,
 
 	###################### weapon.py ######################
