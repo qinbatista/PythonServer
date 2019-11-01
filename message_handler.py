9116339c9f5188a423776672c6b3a1e85f6af447
Matthew Fernandez <mattdfernandez@gmail.com>
2019-11-01 12:04:05 +0800

    renamed purchase_acceleration_factory -> buy_acceleration_factory

--- message_handler.py
+++ message_handler.py
@@ -370,8 +370,8 @@ class MessageHandler:
 	async def _set_armor_factory(self, data: dict) -> str:
 		return await factory.set_armor(data['data']['unique_id'], int(data['data']['aid']), **data)
 
-	async def _purchase_acceleration_factory(self, data: dict) -> str:
-		return await factory.purchase_acceleration(data['data']['unique_id'], **data)
+	async def _buy_acceleration_factory(self, data: dict) -> str:
+		return await factory.buy_acceleration(data['data']['unique_id'], **data)
 
 	async def _get_config_factory(self, data: dict) -> str:
 		return common.mt(0, 'success', data['config']['factory'])
@@ -728,7 +728,7 @@ FUNCTION_LIST = {
 	'decrease_worker_factory' : MessageHandler._decrease_worker_factory,
 	'activate_wishing_pool_factory' : MessageHandler._activate_wishing_pool_factory,
 	'upgrade_wishing_pool_factory' : MessageHandler._upgrade_wishing_pool_factory,
-	'purchase_acceleration_factory' : MessageHandler._purchase_acceleration_factory,
+	'buy_acceleration_factory' : MessageHandler._buy_acceleration_factory,
 	'set_armor_factory' : MessageHandler._set_armor_factory,
 	'get_config_factory' : MessageHandler._get_config_factory,
 
