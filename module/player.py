50f71d624752eaea298802f40857791c79ed4f54
Matthew Fernandez <mattdfernandez@gmail.com>
2019-11-04 11:30:34 +0800

    inserted default values for factory unassigned workers in create function

--- module/player.py
+++ module/player.py
@@ -8,6 +8,8 @@ from module import common
 
 async def create(uid, gn, **kwargs):
 	added, _ = await common.execute_update(f'INSERT INTO player(uid, gn) VALUES ("{uid}", "{gn}") ON DUPLICATE KEY UPDATE gn = gn;', **kwargs)
+	await common.execute(f'INSERT INTO `factory` (`uid`, `fid`, `workers`, `storage`) VALUES \
+			("{uid}", {enums.Factory.UNASSIGNED.value}, 5, 5);', **kwargs)
 	return common.mt(0, 'success', {'gn' : gn}) if added != 0 else common.mt(99, 'gamename or uid already exists')
 
 async def enter_world(uid, **kwargs):
