3c6264c8cfd570d389192837d7b862b6875a6898
Matthew Fernandez <mattdfernandez@gmail.com>
2019-11-05 11:15:24 +0800

    starts new edge server instead of chat_server

--- start_public_server.py
+++ start_public_server.py
@@ -69,7 +69,7 @@ def main():
 		time.sleep(1)
 		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/mail_server.py']))
 		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/token_server.py']))
-		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/chat_server.py']))
+		processes.append(subprocess.Popen([GetPythonCommand(), loc() + '/edge.py']))
 		# processes.append(subprocess.Popen(['python', loc() + '/account_manager.py']))
 		# processes.append(subprocess.Popen(['python', loc() + '/game_manager_qin.py']))
 		# processes.append(subprocess.Popen(['python3', loc() + '/game_manager_houyao.py']))
