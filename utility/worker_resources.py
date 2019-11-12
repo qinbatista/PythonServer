bdc34bfc87e4c8d3688c78ca724468cdaa24fc5f
Matthew Fernandez <mattdfernandez@gmail.com>
2019-11-12 11:14:54 +0800

    removed unused process executor

--- utility/worker_resources.py
+++ utility/worker_resources.py
@@ -12,7 +12,6 @@ import aiohttp
 import aioredis
 import aiomysql
 import requests
-import concurrent
 
 class WorkerResources:
 	def __init__(self, cfg):
@@ -20,7 +19,6 @@ class WorkerResources:
 		self.resources = {}
 	
 	async def init(self):
-		self.resources['executor'] = concurrent.futures.ProcessPoolExecutor(max_workers = 1)
 		self.resources['session'] = aiohttp.ClientSession()
 		self.resources['redis'] = await aioredis.create_redis(self.cfg['redis']['addr'])
 		self.resources['db'] = await aiomysql.create_pool(maxsize = 10, host = '192.168.1.102', user = 'root', password = 'lukseun', charset = 'utf8', autocommit = True, db = 'experimental')
