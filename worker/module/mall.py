06cba29b713cb55547d943d18a564a60b3e5fdb6
houyao <2428437133@qq.com>
2019-11-22 16:04:25 +0800

    完成了人民币商城的方法 添加了商城记录的数据库表

--- worker/module/mall.py
+++ worker/module/mall.py
@@ -2,14 +2,52 @@
 task.py
 '''
 
-from module import enums
-from module import common
-from module import vip
+from worker.module import enums
+from worker.module import common
+from worker.module import vip
 from datetime import datetime
 import time
-from module import task
 
 
+async def rmb_mall(key, **kwargs):
+	"""
+	参数详解：
+	cty：消耗品类型       包含 钻石diamond，金币coin，人民币rmb
+	ity：物品类型         包含 消耗品consume，装饰品decoration，卡片物品card
+	iid：物品id           包含 item枚举下的所有可以购买的物品id
+	gty：礼包类型         包含 1个购买"1"，10个购买"10"，30个购买"30"
+	qty：礼包购买数量     代表购买几个相同的礼包
+	配置结构：
+	cty:(diamond、coin、rmb)
+		- cid(消耗品的item id，rmb为-1)
+		- merchandise:(商品信息)
+			- ity:(物品类型暂时分为 消耗品consume和装饰品decoration)
+				- iid:(物品iid，根据item枚举设置)
+					- gty:(礼包类型，暂时分为1个购买"1"，10个购买"10"，30个购买"30")
+						c_quantity(消耗品数量)
+						m_quantity(商品数量)
+	数据返回格式：
+	0 - success
+	96 - 物品{iid}礼包{gty}类型缺失商品数量
+	97 - 物品{iid}没有礼包{gty}类型
+	98 - 没有出售此物品{iid}
+	99 - 物品类型错误{ity}
+	"""
+	uid, ity, iid, gty, qty = decode_key(key)  # str, str, str, int
+	config = kwargs['config']['mall']['rmb']['merchandise'].get(ity, False)
+	if not config: return common.mt(99, f'物品类型错误{ity}')
+	merchandise = config.get(iid, False)
+	if not merchandise: return common.mt(98, f'没有出售此物品{iid}')
+	commodity = merchandise.get(gty, False)
+	if not commodity: return common.mt(97, f'物品{iid}没有礼包{gty}类型')
+	m_quantity = commodity.get('m_quantity', False)
+	if not m_quantity: return common.mt(96, f'物品{iid}礼包{gty}类型缺失商品数量')
+	qty = abs(qty * m_quantity)
+	if ity == 'card':
+		for _ in range(qty): await vip.buy_card(uid, int(iid), **kwargs)
+	else:
+		await common.try_item(uid, iid, qty, **kwargs)
+	return common.mt(0, 'success')
 
 
 ######################################################### 私有 #########################################################
@@ -64,44 +102,6 @@ async def mall(uid, cty, ity, iid, gty, qty, **kwargs):
 	# await self._execute_statement_update(world=world, statement=f"update player set {scroll_type}={scroll_type}+{mall_scroll_quantity} where unique_id='{unique_id}'")
 	# scroll_quantity = await self._get_material(world=world, unique_id=unique_id, material=scroll_type)
 	# return self._message_typesetting(status=0, message="Purchase success", data={"remaining": {mall_consume: consume_data["remaining"], scroll_type: scroll_quantity}, "reward": {scroll_type: mall_scroll_quantity}})
-
-
-async def rmb_mall(key, **kwargs):
-	"""
-	参数详解：
-	cty：消耗品类型       包含 钻石diamond，金币coin，人民币rmb
-	ity：物品类型         包含 消耗品consume，装饰品decoration，卡片物品card
-	iid：物品id           包含 item枚举下的所有可以购买的物品id
-	gty：礼包类型         包含 1个购买"1"，10个购买"10"，30个购买"30"
-	qty：礼包购买数量     代表购买几个相同的礼包
-	配置结构：
-	cty:(diamond、coin、rmb)
-		- cid(消耗品的item id，rmb为-1)
-		- merchandise:(商品信息)
-			- ity:(物品类型暂时分为 消耗品consume和装饰品decoration)
-				- iid:(物品iid，根据item枚举设置)
-					- gty:(礼包类型，暂时分为1个购买"1"，10个购买"10"，30个购买"30")
-						c_quantity(消耗品数量)
-						m_quantity(商品数量)
-	数据返回格式：
-	0 - Purchase success
-	96 - 物品{iid}礼包{gty}类型缺失商品数量
-	97 - 物品{iid}没有礼包{gty}类型
-	98 - 没有出售此物品{iid}
-	99 - 物品类型错误{ity}
-	"""
-	uid, ity, iid, gty, qty = decode_key(key)  # str, str, str, int
-	config = kwargs['config']['mall']['rmb']['merchandise'].get(ity, False)
-	if not config: return common.mt(99, f'物品类型错误{ity}')
-	merchandise = config.get(iid, False)
-	if not merchandise: return common.mt(98, f'没有出售此物品{iid}')
-	commodity = merchandise.get(gty, False)
-	if not commodity: return common.mt(97, f'物品{iid}没有礼包{gty}类型')
-	m_quantity = commodity.get('m_quantity', False)
-	if not m_quantity: return common.mt(96, f'物品{iid}礼包{gty}类型缺失商品数量')
-	qty = abs(qty * m_quantity)
-	if ity == 'card':
-		for i in qty: await vip.buy_card(uid, int(iid))
 
 
 def decode_key(key):
