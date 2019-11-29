337a58f46dadd5dd6b43612eed9a7c96bbc96eac
Matthew Fernandez <mattdfernandez@gmail.com>
2019-11-29 09:44:20 +0800

    added flags to specify database addr, user, and pw

--- worker/worker.py
+++ worker/worker.py
@@ -34,7 +34,8 @@ class Worker:
 		self.running = False
 
 		self.configs = worker_resources.ModuleConfigurations()
-		self.resource= worker_resources.WorkerResources(self.args.redis_addr)
+		self.resource= worker_resources.WorkerResources(self.args.redis_addr, self.args.db_addr, \
+				self.args.db_user, self.args.db_pw)
 		self.sid     = None
 		self.nats    = None
 
@@ -138,6 +139,9 @@ async def main():
 	parser.add_argument('--token-addr', type = str, default = 'http://token')
 	parser.add_argument('--mail-port' , type = int, default = 8020)
 	parser.add_argument('--token-port', type = int, default = 8001)
+	parser.add_argument('--db-addr'   , type = str, default = '192.168.1.102')
+	parser.add_argument('--db-user'   , type = str, default = 'root')
+	parser.add_argument('--db-pw'     , type = str, default = 'lukseun')
 	await Worker(parser.parse_args()).start(debug=True)
 
 if __name__ == '__main__':
