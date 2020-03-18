1f5ecdb8455532748f0cf984ff5045d5001fa835
houyao <2428437133@qq.com>
2020-03-18 11:33:23 +0800

    完善了MySQL表单的创建

--- utility/user_creator.py
+++ utility/user_creator.py
@@ -4,11 +4,13 @@ user_creator.py
 
 import pymysql
 import argparse
+import sys
+import contextlib
 
 
 def create_table_info(cursor):
-	statement = \
-	"""
+    statement = \
+        """
 	CREATE TABLE `info` (
 	  `unique_id` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
 	  `cuid` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '提供客户端的uid',
@@ -24,38 +26,33 @@ def create_table_info(cursor):
 	  UNIQUE KEY `c_uid` (`cuid`)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 	"""
-	cursor.execute(statement)
+    cursor.execute(statement)
 
-def create_user(mysql_addr, mysql_user, mysql_pw):
-	c = pymysql.connect(host = mysql_addr, user = mysql_user, password = mysql_pw, \
-			charset = 'utf8mb4', autocommit=True)
-	cursor = c.cursor()
-	cursor.execute(f'CREATE DATABASE `user`;')
-	c.select_db('user')
-	cursor = c.cursor()
-	create_table_info(cursor)
 
-def already_exists(mysql_addr, mysql_user, mysql_pw):
-	try:
-		pymysql.connect(host = mysql_addr, user = mysql_user, password = mysql_pw, \
-				charset = 'utf8mb4', db = 'user')
-	except pymysql.err.InternalError as e:
-		code, msg = e.args
-		if code == 1049: return False
-	return True
+def create_user(mysql_addr, mysql_user, mysql_pw, port):
+    c = pymysql.connect(host=mysql_addr, user=mysql_user, password=mysql_pw,
+                        port=port,
+                        charset='utf8mb4', autocommit=True)
+    cursor = c.cursor()
+    with contextlib.suppress(pymysql.err.ProgrammingError):
+        cursor.execute(f'CREATE DATABASE `user`;')
+    c.select_db('user')
+    cursor = c.cursor()
+    with contextlib.suppress(pymysql.err.InternalError):
+        create_table_info(cursor)
+
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--addr', type = str, default = '192.168.1.102')
-	parser.add_argument('--pwrd', type = str, default = 'lukseun')
-	parser.add_argument('--user', type = str, default = 'root')
-	args = parser.parse_args()
-
-	if not already_exists(args.addr, args.user, args.pwrd):
-		create_user(args.addr, args.user, args.pwrd)
-	else:
-		print('User table already exists')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--addr', type=str, default='192.168.1.143')
+    parser.add_argument('--pwrd', type=str, default='lukseun')
+    parser.add_argument('--user', type=str, default='root')
+    parser.add_argument('--port', type=int, default=3306)
+    args = parser.parse_args()
+    create_user(args.addr, args.user, args.pwrd, args.port)
 
 
 if __name__ == '__main__':
-	main()
+    if len(sys.argv) == 1:
+        sys.argv.extend(['--addr', '192.168.1.143', '--port', '3307'])
+    main()
--- utility/world_creator.py
+++ utility/world_creator.py
@@ -3,118 +3,119 @@ world_creator.py
 '''
 
 import os
+import sys
 import json
 import mailbox
 import pymysql
 import argparse
+import contextlib
 
 # imporant the create statements for each table
 # statement names are imported to the local namespace, so be careful with name collisions
-from world_structure import *  
+from world_structure import *
 
 TABLES = [PLAYER, SUMMON, ACHIEVEMENT, ARMOR, CHECKIN, DARKMARKET,
-		FACTORY, FAMILY, FAMILYHISTORY, FAMILYROLE, FRIEND, ITEM, LEADERBOARD, LIMITS,
-		PROGRESS, ROLE, ROLEPASSIVE, SKILL, TASK, TIMER, WEAPON, WEAPONPASSIVE,
-		CONSTRAINT, TRIGGER1, TRIGGER2]
+          FACTORY, FAMILY, FAMILYHISTORY, FAMILYROLE, FRIEND, ITEM,
+          LEADERBOARD, LIMITS,
+          PROGRESS, ROLE, ROLEPASSIVE, SKILL, TASK, TIMER, WEAPON,
+          WEAPONPASSIVE,
+          CONSTRAINT, TRIGGER1, TRIGGER2]
+
 
 #########################################################################################
 
-def database_exists(db, mysql_addr, mysql_user, mysql_pw):
-	try:
-		pymysql.connect(host = mysql_addr, user = mysql_user, password = mysql_pw,
-				charset = 'utf8mb4', db = db)
-	except pymysql.err.InternalError as e:
-		if e.args[0] == 1049: return False
-	return True
+def get_conn(mysql_addr, mysql_user, mysql_pw, port):
+    return pymysql.connect(host=mysql_addr, user=mysql_user, password=mysql_pw,
+                           charset='utf8mb4', port=port, autocommit=True)
 
-def create_db(world, mysql_addr, mysql_user, mysql_pw):
-	if not database_exists(world, mysql_addr, mysql_user, mysql_pw):
-		connection = pymysql.connect(host = mysql_addr, user = mysql_user,
-				password = mysql_pw, charset = 'utf8mb4', autocommit = True)
-		connection.cursor().execute(f'CREATE DATABASE `{world}`;')
-		connection.select_db(world)
-		# connection.cursor().execute(MALL)
-		for table in TABLES:
-			connection.cursor().execute(table)
-		print(f'created new database for world {world}..')
-	else:
-		print(f'database for world {world} already exists, skipping..')
-		connection = pymysql.connect(host = mysql_addr, user = mysql_user,
-				password = mysql_pw, charset = 'utf8mb4', autocommit = True)
-		connection.select_db(world)
-		for table in TABLES:
-			try:
-				connection.cursor().execute(table)
-			except:
-				print("error")
-		print(f'created new database for world {world}..')
+
+def create_db(world, mysql_addr, mysql_user, mysql_pw, port):
+    conn = get_conn(mysql_addr, mysql_user, mysql_pw, port)
+    with contextlib.suppress(pymysql.err.ProgrammingError):
+        conn.cursor().execute(f'CREATE DATABASE `{world}`;')
+    conn.select_db(world)
+    cursor = conn.cursor()
+    for table in TABLES:
+        with contextlib.suppress(pymysql.err.InternalError):
+            cursor.execute(table)
+    print(f'created new database for world {world}..')
 
 
 def create_mailbox(world):
-	path = os.path.dirname(os.path.realpath(__file__)) + '/../box'
-	box  = mailbox.Maildir(path)
-	try:
-		worldbox = box.get_folder(str(world))
-		print(f'mailbox for world {world} already exists, skipping..')
-	except mailbox.NoSuchMailboxError:
-		box.add_folder(str(world))
-		print(f'added mailbox for world {world}..')
+    path = os.path.dirname(os.path.realpath(__file__)) + '/../box'
+    box = mailbox.Maildir(path)
+    try:
+        world_box = box.get_folder(str(world))
+        print(f'mailbox for world {world} already exists, skipping..')
+    except mailbox.NoSuchMailboxError:
+        box.add_folder(str(world))
+        print(f'added mailbox for world {world}..')
 
-def create_world(world, mysql_addr, mysql_user, mysql_pw):
-	create_db(world, mysql_addr, mysql_user, mysql_pw)
-	create_mailbox(world)
-	# TODO update path / make this compatible with aliyun mounted NAS
+
+def create_world(world, mysql_addr, mysql_user, mysql_pw, port):
+    create_db(world, mysql_addr, mysql_user, mysql_pw, port)
+    create_mailbox(world)
+
+
+# TODO update path / make this compatible with aliyun mounted NAS
 
 
 def loc():
-	return os.path.dirname(os.path.realpath(__file__))
+    return os.path.dirname(os.path.realpath(__file__))
 
 
 def save_world_config(world, path):
-	data = json.load(open(path, encoding='utf-8'))
-	data['worlds'].append({
-		"status" : 0,
-		"id"     : world,
-		"name"   : f"world {world}"
-	})
-	with open(path, 'w', encoding='utf-8') as f:
-		f.write(json.dumps(data))
-
+    data = json.load(open(path, encoding='utf-8'))
+    data['worlds'].append({
+        "status": 0,
+        "id": world,
+        "name": f"world {world}",
+        "merge": world
+    })
+    worlds = []
+    for wd in data['worlds']:
+        if wd not in worlds:
+            worlds.append(wd)
+    data['worlds'] = worlds
+    with open(path, 'w', encoding='utf-8') as w:
+        json.dump(data, w, ensure_ascii=False, indent=4)
 
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--addr', type = str, default = '192.168.1.102')
-	parser.add_argument('--pwrd', type = str, default = 'lukseun')
-	parser.add_argument('--user', type = str, default = 'root')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--addr', type=str, default='192.168.1.143')
+    parser.add_argument('--pwrd', type=str, default='lukseun')
+    parser.add_argument('--user', type=str, default='root')
+    parser.add_argument('--port', type=int, default=3306)
+    args = parser.parse_args()
 
-	path = os.path.join(loc(), '../config/configuration/1.0/server/world.json')
-	for i in range(0, 10):
-		world = f's{i}'
-		create_world(world, args.addr, args.user, args.pwrd)
-		save_world_config(world, path)
+    path = os.path.join(loc(), '../config/configuration/1.0/server/world.json')
+    for i in range(0, 10):
+        world = f's{i}'
+        create_world(world, args.addr, args.user, args.pwrd, args.port)
+        save_world_config(world, path)
 
 
-def test(world, mysql_addr, mysql_user, mysql_pw):
-	connection = pymysql.connect(host=mysql_addr, user=mysql_user, password=mysql_pw, charset='utf8mb4', autocommit=True)
-	connection.cursor().execute(f'CREATE DATABASE `{world}`;')
-	connection.select_db(world)
-
-	for table in TABLES:
-		connection.cursor().execute(table)
-	"""
-	cursor = connection.cursor()
-	cursor.execute(f"SELECT * FROM player;")
-	data = cursor.fetchall()
-	for d in data:
-		if d[2] is None:
-			print(f'{d} is None')
-		else:
-			print(f'{d}')
-	"""
+def test(world, mysql_addr, mysql_user, mysql_pw, port=3307):
+    conn = get_conn(mysql_addr, mysql_user, mysql_pw, port)
+    conn.cursor().execute(f'CREATE DATABASE `{world}`;')
+    conn.select_db(world)
+    for table in TABLES:
+        conn.cursor().execute(table)
+    """
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM player;")
+    data = cursor.fetchall()
+    for d in data:
+        if d[2] is None:
+            print(f'{d} is None')
+        else:
+            print(f'{d}')
+    """
 
 
 if __name__ == '__main__':
-	main()
-	# test("s6", "192.168.1.102", "root", "lukseun")
+    if len(sys.argv) == 1:
+        sys.argv.extend(['--addr', '192.168.1.143', '--port', '3307'])
+    main()
+    # test("s6", "192.168.1.143", "root", "lukseun")
